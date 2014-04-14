57626f5b856376f6356997cce3a0601149313209
Vinny Crupi <vinnycrupi@hotmail.com>
2014-04-14 00:38:51 -0400

    go away, comments

--- Sportsapalooza/src/com/gamemen/sportsapalooza/Audio.java
+++ Sportsapalooza/src/com/gamemen/sportsapalooza/Audio.java
@@ -5,19 +5,11 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 
 public class Audio {
-//	public static MediaPlayer countdown, teeoff, button, crowd, bounce, explosion;
 	private static SoundPool pool;
 	private static int volume = 100;
 	public static int countdown, teeoff, button, crowd, bounce, explosion;
 	
 	public static void loadAudio(Context context) {
-//		countdown = MediaPlayer.create(context, R.raw.countdown);
-//		teeoff = MediaPlayer.create(context, R.raw.teeoff);
-//		button = MediaPlayer.create(context, R.raw.button);
-//		crowd = MediaPlayer.create(context, R.raw.crowd);
-//		bounce = MediaPlayer.create(context, R.raw.bounce);
-//		explosion = MediaPlayer.create(context, R.raw.explosion);
-		
 		pool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
 		
 		countdown = pool.load(context, R.raw.countdown, 1);
@@ -29,25 +21,10 @@ public class Audio {
 	}
 	
 	public static void release() {
-//		MediaPlayer[] players = { countdown, teeoff, button, crowd, bounce, explosion };
-//		
-//		for (int i = 0; i < players.length; ++i) {
-//			players[i].reset();
-//			players[i].release();
-//			players[i] = null;
-//		}
-		
 		pool.release();
 	}
 	
 	public static void play(int soundID) {
-//		if (player.isPlaying()) {
-//			MediaPlayer copy = new MediaPlayer(player);
-//		}
-//		else {
-//			player.start();
-//		}
-		
 		pool.play(soundID, volume, volume, 1, 0, 1);
 	}
 	
